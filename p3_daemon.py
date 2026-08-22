c59aa0b4429e9b78ba95ed8036e2aa00638f9f54
23emrahgunes <168855296+23emrahgunes@users.noreply.github.com>
2026-08-22 22:57:02 +0300

    Use reconnect-aware P3 scanner in runtime daemon

--- p3_daemon.py
+++ p3_daemon.py
@@ -13,7 +13,7 @@ import time
 
 from p3_config import get_p3_settings
 from p3_replay_scheduler import P3ReplayEngine
-from p3_scanner import StructuralArbScanner
+from p3_scanner_resilient import ReconnectAwareStructuralArbScanner as StructuralArbScanner
 from p3_web import run_web
 
 
